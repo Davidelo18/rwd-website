86f93197064fb9d27f6655454e83725742acc8d8
davidelo <daw.kielbik@gmail.com>
2018-11-05 22:53:45 +0100

    added an animation to slider

--- scipts/slider.js
+++ scipts/slider.js
@@ -1,48 +1,53 @@
 let sliderImages = document.querySelectorAll('.slide'); //catch all the slides
 let arrowLeft = document.querySelector('#arrow-left');
 let arrowRight = document.querySelector('#arrow-right');
-let currentImage = 0; //varieble that will tell the site which img is now visible
+let currentImage = 0; //variable that tell which img is now visible by index starting from 0 to length of sliderImages array
 
-//clears all the images
-function resetSlider() {
-    for (let i = 0; i < sliderImages.length; i++) {
-        sliderImages[i].style.display = 'none';
-    }
-}
-
-//function that starts right after the page load - start the slider from the first (0 index) pic
-function startSlide() {
-    resetSlider();
-    sliderImages[0].style.display = 'block';
-}
-
-//fuctions to slide back and next images
+//fuctions to slide previous and next images
 function slideBack() {
-    resetSlider();
-    sliderImages[currentImage - 1].style.display = 'block';
     currentImage--;
+    if(currentImage < 0){
+        currentImage = sliderImages.length;
+        sliderImages[0].classList.remove('block-show');
+        sliderImages[0].style.display = 'none';
+        sliderImages[sliderImages.length - 1].style.display = 'block';
+        sliderImages[sliderImages.length - 1].classList.add('block-show');
+        currentImage = sliderImages.length - 1;
+    } else{
+        sliderImages[currentImage + 1].classList.remove('block-show');
+        sliderImages[currentImage + 1].style.display = 'none';
+        sliderImages[currentImage].style.display = 'block';
+        sliderImages[currentImage].classList.add('block-show');  
+    }    
 }
 
 function slideNext() {
-    resetSlider();
-    sliderImages[currentImage + 1].style.display = 'block';
     currentImage++;
+    if(currentImage === sliderImages.length ){
+        currentImage = 0;
+        sliderImages[sliderImages.length - 1].classList.remove('block-show');
+        sliderImages[sliderImages.length - 1].style.display = 'none';
+    } else{
+        sliderImages[currentImage - 1].classList.remove('block-show');
+        sliderImages[currentImage - 1].style.display = 'none';
+    }
+    sliderImages[currentImage].style.display = 'block';
+    sliderImages[currentImage].classList.add('block-show');
 }
 
 // events to slide images after click left of right arrow
 arrowLeft.addEventListener('click', function () {
-    if (currentImage === 0) {
-        currentImage = sliderImages.length;
-    }
     slideBack();
+    clearInterval(myTimer);
+    myTimer = setInterval("slideNext()", 5000);;
 })
 
 arrowRight.addEventListener('click', function () {
-    if (currentImage === sliderImages.length - 1) {
-        currentImage = -1;
-    }
     slideNext();
+    clearInterval(myTimer);
+    myTimer = setInterval("slideNext()", 5000);;
 })
 
-//start after the site loading finished 
-startSlide();
+// images automaticly switch after loading the website
+// clicking on any arrow reset the timer
+let myTimer = setInterval("slideNext()", 5000);
